ea8df54cecd2b6ad560bfbb2a704608d7b66a85e
Nitin Gupta <nitigupt@adobe.com>
2024-03-05 13:53:43 +0530

    mutation observer

---
+++ blocks/form/form-editor-support.js
@@ -0,0 +1,10 @@
+//var insertedNodes = [];
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        for(var i = 0; i < mutation.addedNodes.length; i++)
+            console.log(mutation.addedNodes[i]);
+    })
+});
+observer.observe(document, {
+    childList: true
+});
